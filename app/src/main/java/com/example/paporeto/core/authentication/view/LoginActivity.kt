7a875f79d96b1af34971e20893ed861edee27fad
Lucas Lopes <lucaslopesdemelosilva@gmail.com>
2024-02-14 16:46:54 -0300

    Animacoes dos fragmentos

--- app/src/main/java/com/example/paporeto/core/authentication/view/LoginActivity.kt
+++ app/src/main/java/com/example/paporeto/core/authentication/view/LoginActivity.kt
@@ -18,6 +18,12 @@ class LoginActivity : AppCompatActivity(), LoginCallback {
     override fun onCreateAcountClicked() {
         val fragment = SignUpFragment()
         supportFragmentManager.commit {
+            setCustomAnimations(
+                R.anim.slide_in,
+                R.anim.fade_out,
+                R.anim.fade_in,
+                R.anim.slide_out
+            )
             replace(R.id.fragment_container_view, fragment)
             addToBackStack(null)
         }
